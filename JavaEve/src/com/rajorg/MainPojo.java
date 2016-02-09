06f4ced60522c61e5c4f53da1d1faf60401e77fe
JavaAnalyst <bobbieanalyst@outlook.com>
2016-02-08 22:08:00 -0500

    Commit at push

--- JavaEve/src/com/rajorg/MainPojo.java
+++ JavaEve/src/com/rajorg/MainPojo.java
@@ -10,8 +10,18 @@ public class MainPojo {
 		pdemo.setSalary(4200.0f);
 		pdemo.setTypeOfWork("Java Programmer");
 		pdemo.setIsWorking(true);
-		System.out.println(pdemo.Age+" "+pdemo.Fname);
+		// System.out.println(pdemo.Age + " " + pdemo.Fname);
+
+		ScCalculator scal = new ScCalculator();
+
+		// System.out.println(scal.cube(5, 3, 5));
+		// System.out.println(scal.Square(5, 3));
+
+		RCalculator rcal = new RCalculator();
+		// System.out.println(rcal.Add(2, 5, 6));
+
+		// System.out.println(rcal.Mulitply(3, 4, 2));
+
 	}
-	
 
 }
